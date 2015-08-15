c514c4abccf6d983f0eb9f1f2123323723b59bb3
Alfredo Rius <alfredo.rius@gmail.com>
2015-08-15 17:33:40 -0500

    Removed ImageStyle from the admin site

--- image_styles/admin.py
+++ image_styles/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from image_styles.models import *
 
-admin.site.register(ImageStyle)
 admin.site.register(Style)
 admin.site.register(Crop)
 admin.site.register(Enhance)
