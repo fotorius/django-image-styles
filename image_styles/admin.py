5ac2bd11ebf206fae4f9c0deeb2ac5cd56a454a9
Alfredo Rius <alfredo.rius@gmail.com>
2015-08-18 12:18:34 -0500

    Removed django admin file admin.py

--- image_styles/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from image_styles.models import *
-
-admin.site.register(Style)
-admin.site.register(Crop)
-admin.site.register(Enhance)
-admin.site.register(Resize)
-admin.site.register(Scale)
-admin.site.register(Rotate)
