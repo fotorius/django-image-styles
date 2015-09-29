359cde1d59ca7aab8be3c3048362d80cfc1a794f
devalfrz <alfredo.rius@gmail.com>
2015-09-28 21:18:15 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django Image Styles',
+      version='1.0',
+      description='Django image management utility',
+      author='Alfredo Rius',
+      author_email='alfredo.rius@behuns.com',
+      url='http://behuns.com',
+      packages=['image_styles'],
+     )
