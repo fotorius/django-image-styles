1dc6c4d83bb15725e48ad5a8b1025251e2ecd822
devalfrz <alfredo.rius@gmail.com>
2015-09-28 21:53:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-#!/usr/bin/env python
+import os
+from setuptools import setup
 
-from distutils.core import setup
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
-setup(name='Django Image Styles',
-      version='1.0',
-      description='Django image management utility',
-      author='Alfredo Rius',
-      author_email='alfredo.rius@behuns.com',
-      url='http://behuns.com',
-      packages=['image_styles'],
-     )
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-image-styles',
+    version='1.0',
+    packages=['image_styles'],
+    include_package_data=True,
+    license='GPL License',
+    description='Django image management utility',
+    long_description=README,
+    url='http://behuns.com',
+    author='Alfredo Rius',
+    author_email='alfredo.rius@behuns.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
